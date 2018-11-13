974f408fdc718e9e8c35b14fe16f26809149979e
wtysos11 <wtysos11@163.com>
2018-11-13 18:00:26 +0800

    static file server using gorilla/mux and negroni

--- main.go
+++ main.go
@@ -1,41 +1,23 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
-	"log"
 	"net/http"
-	"strings"
+
+	"github.com/gorilla/mux"
+	"github.com/urfave/negroni"
 )
 
-func sayhelloName(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()       //解析参数，默认是不会解析的
-	fmt.Println(r.Form) //这些信息是输出到服务器端的打印信息
-	fmt.Println("path", r.URL.Path)
-	fmt.Println("scheme", r.URL.Scheme)
-	fmt.Println(r.Form["url_long"])
-	for k, v := range r.Form {
-		fmt.Println("key:", k)
-		fmt.Println("val:", strings.Join(v, ""))
-	}
-	fmt.Fprintf(w, "Hello astaxie!") //这个写入到w的是输出到客户端的
-}
-
-func fileHandler(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()
-	path := r.URL.Path[1:]
-	body, err := ioutil.ReadFile(path)
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Fprintf(w, string(body))
+func addStaticFileServer(r *mux.Router) {
+	dir := "file"
+	r.PathPrefix("/").Handler(http.StripPrefix("/", http.FileServer(http.Dir(dir))))
 }
 
 func main() {
-	http.HandleFunc("/", sayhelloName)
-	http.HandleFunc("/file/", fileHandler)
-	err := http.ListenAndServe(":8080", nil) //设置监听的端口
-	if err != nil {
-		log.Fatal("ListenAndServe: ", err)
-	}
+	n := negroni.Classic()
+	router := mux.NewRouter()
+	//router.HandleFunc("/", )
+	addStaticFileServer(router)
+	// router goes last
+	n.UseHandler(router)
+	http.ListenAndServe(":8080", n)
 }
