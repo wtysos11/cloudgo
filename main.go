7d25667d34c1b791168fa75d4db3f23333d2a6c6
wtysos11 <wtysos11@163.com>
2018-11-14 10:03:25 +0800

    combine render and api

--- main.go
+++ main.go
@@ -25,13 +25,28 @@ func apiTestHandler(formatter *render.Render) http.HandlerFunc {
 	}
 }
 
+func renderTestHandler(formatter *render.Render) http.HandlerFunc {
+	return func(w http.ResponseWriter, req *http.Request) {
+		vars := mux.Vars(req)
+		id := vars["id"]
+		content := vars["content"]
+		formatter.HTML(w, http.StatusOK, "template", struct {
+			ID      string `json:"id"`
+			Content string `json:"content"`
+		}{ID: id, Content: content})
+	}
+}
+
 func main() {
 	n := negroni.Classic()
 	router := mux.NewRouter()
 	formatter := render.New(render.Options{
 		IndentJSON: true,
+		Directory:  "file",
+		Extensions: []string{".html"},
 	})
 	router.HandleFunc("/api/{id}/{content}", apiTestHandler(formatter))
+	router.HandleFunc("/render/{id}/{content}", renderTestHandler(formatter))
 	addStaticFileServer(router)
 	// router goes last
 	n.UseHandler(router)
