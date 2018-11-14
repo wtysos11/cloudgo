d5f6178ae3951e154372391eb372503f23e39750
wtysos11 <wtysos11@163.com>
2018-11-14 10:38:49 +0800

    finish handle to /unknown

--- main.go
+++ main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"net/http"
 	"fmt"
+	"net/http"
+
 	"github.com/gorilla/mux"
 	"github.com/unrolled/render"
 	"github.com/urfave/negroni"
@@ -36,27 +37,31 @@ func renderTestHandler(formatter *render.Render) http.HandlerFunc {
 		}{ID: id, Content: content})
 	}
 }
-func loginHandler(formatter *render.Render) http.HandlerFunc{
-	return func(w http.ResponseWriter, r *http.Request){
+func loginHandler(formatter *render.Render) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
 		r.ParseForm()
 		fmt.Println("method:", r.Method) //获取请求的方法
 		fmt.Println("username:", r.Form["username"])
 		fmt.Println("password:", r.Form["password"])
-		if(r.Method == "GET"){
-			fmt.Fprintf(w,"Can't directly get the page, you should go to index.html first")
-		}else{
-			if len(r.Form["username"][0])!=0{
+		if r.Method == "GET" {
+			fmt.Fprintf(w, "Can't directly get the page, you should go to index.html first")
+		} else {
+			if len(r.Form["username"][0]) != 0 {
 				formatter.HTML(w, http.StatusOK, "form", struct {
-					USER	string `json:"user"`
-					PASS	string `json:"password"`
+					USER string `json:"user"`
+					PASS string `json:"password"`
 				}{USER: r.Form["username"][0], PASS: r.Form["password"][0]})
 			}
 		}
-
-
 	}
 }
 
+func developingHandler(formatter *render.Render) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("500 - Somthing bad happened!"))
+	}
+}
 
 func main() {
 	n := negroni.Classic()
@@ -66,10 +71,11 @@ func main() {
 		Directory:  "file",
 		Extensions: []string{".html"},
 	})
-	
+
 	router.HandleFunc("/api/{id}/{content}", apiTestHandler(formatter))
 	router.HandleFunc("/render/{id}/{content}", renderTestHandler(formatter))
-	router.HandleFunc("/login",loginHandler(formatter))
+	router.HandleFunc("/login", loginHandler(formatter))
+	router.HandleFunc("/unknown", developingHandler(formatter))
 	addStaticFileServer(router)
 	// router goes last
 	n.UseHandler(router)
